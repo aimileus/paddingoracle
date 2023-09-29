991171c5d21b24d2c9a835760ef5f11deda99d3b
Emiel Wiedijk <13463911+aimileus@users.noreply.github.com>
2023-09-29 19:12:09 +0200

    Initial commit

---
+++ aes.py
@@ -0,0 +1,54 @@
+from secrets import token_bytes
+from string import ascii_lowercase
+
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+BLOCK_LENGTH = 16
+
+
+def cbc(key: bytes, iv: bytes) -> Cipher:
+    return Cipher(algorithms.AES(key), modes.CBC(iv))
+
+
+def decrypt(k: bytes, c: bytes):
+    iv = c[:BLOCK_LENGTH]
+    b1 = c[BLOCK_LENGTH:]
+
+    cipher: Cipher = cbc(k, iv)
+    decryptor = cipher.decryptor()
+
+    plain = decryptor.update(b1) + decryptor.finalize()
+
+    assert len(plain) == len(c) - BLOCK_LENGTH
+
+    return plain
+
+
+def encrypt(k: bytes, m: bytes):
+    iv = token_bytes(BLOCK_LENGTH)
+
+    cipher = cbc(k, iv)
+    encryptor = cipher.encryptor()
+    return iv + encryptor.update(m) + encryptor.finalize()
+
+
+def pad(m: bytes):
+    padder = padding.PKCS7(BLOCK_LENGTH * 8).padder()
+    return padder.update(m) + padder.finalize()
+
+
+def valid_cipher(k: bytes, c: bytes) -> bool:
+    m = decrypt(k, c)
+    return valid_padding(m)
+
+
+def valid_padding(m: bytes) -> bool:
+    unpadder = padding.PKCS7(BLOCK_LENGTH * 8).unpadder()
+
+    try:
+        _unpadded = unpadder.update(m) + unpadder.finalize()
+    except ValueError:
+        return False
+    else:
+        return True
---
+++ server.py
@@ -0,0 +1,81 @@
+import os
+import socket
+import traceback
+from secrets import token_bytes
+from typing import Callable
+
+from aes import valid_cipher, encrypt, pad, BLOCK_LENGTH
+
+
+def answer(oracle: Callable[[bytes], bytes], conn: socket.socket):
+    while True:
+        if not (raw_length := conn.recv(1)):
+            return
+
+        num_blocks = raw_length[0]
+
+        if num_blocks == 0:
+            return
+
+        c = conn.recv(num_blocks * BLOCK_LENGTH + 1)
+
+        if c[-1] != 0:
+            raise ValueError("Message does not end with 0")
+
+        if oracle(c[:-1]):
+            conn.send(b"1\x00")
+        else:
+            conn.send(b"0\x00")
+
+
+def main():
+    k = read_key()
+
+    def oracle(c):
+        return valid_cipher(k, c)
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    ip = "0.0.0.0"
+    port = 8080
+
+    soc.bind((ip, port))
+    print(f"Listening on {ip}:{port}")
+    soc.listen()
+
+    while True:
+        conn, (addr, port) = soc.accept()
+        print(f"Opened connection from {addr}")
+
+        try:
+            answer(oracle, conn)
+        except Exception:
+            traceback.print_exc()
+        finally:
+            conn.close()
+
+
+def read_key():
+    try:
+        hex_key = os.environ["MC_PADDING_KEY"]
+
+        k = bytes.fromhex(hex_key)
+        print(f"Using configured key:", k.hex())
+
+        assert len(k) == 16
+
+    except KeyError:
+        k = token_bytes(16)
+        print(f"Using random key", k.hex())
+
+    message = os.environ.get("MC_PADDING_MESSAGE", None)
+    if message:
+        padded = pad(message.encode())
+        print("Encrypted message:", encrypt(k, padded).hex())
+
+    return k
+
+
+if __name__ == "__main__":
+    main()
