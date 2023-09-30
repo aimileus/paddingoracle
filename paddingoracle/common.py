476d6b68deeea661574bb657a27ef189cecd8add
Emiel Wiedijk <13463911+aimileus@users.noreply.github.com>
2023-09-30 08:59:54 +0200

    Make server asynchronous

---
+++ paddingoracle/common.py
@@ -0,0 +1,25 @@
+import os
+from secrets import token_bytes
+
+from paddingoracle.aes import pad, encrypt
+
+
+def read_key():
+    try:
+        hex_key = os.environ["MC_PADDING_KEY"]
+
+        k = bytes.fromhex(hex_key)
+        print(f"Using configured key:", k.hex())
+
+        assert len(k) == 16
+
+    except KeyError:
+        k = token_bytes(16)
+        print(f"Using random key", k.hex())
+
+    message = os.environ.get("MC_PADDING_MESSAGE", None)
+    if message:
+        padded = pad(message.encode())
+        print("Encrypted message:", encrypt(k, padded).hex())
+
+    return k
--- paddingoracle/server.py
+++ paddingoracle/server.py
@@ -1,85 +1,65 @@
-import os
-import socket
-import traceback
-from secrets import token_bytes
-from threading import Thread
+import asyncio
 from typing import Callable
 
-from paddingoracle.aes import valid_cipher, encrypt, pad, BLOCK_LENGTH
+from paddingoracle.aes import valid_cipher, BLOCK_LENGTH
+from paddingoracle.common import read_key
 
 
-def send_answer(conn: socket, oracle: Callable[[bytes], bool]):
+async def handle_oracle(oracle: Callable[[bytes], bool], reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    addr, _port = writer.get_extra_info('peername')
+    print("Opened connection from", addr)
+
     while True:
-        raw_length = conn.recv(1)
+        raw_length = await reader.read(1)
         if not raw_length:
-            return
+            break
 
         num_blocks = raw_length[0]
 
         if num_blocks == 0:
-            return
+            break
 
-        c = conn.recv(num_blocks * BLOCK_LENGTH + 1)
+        try:
+            c = await reader.readexactly(num_blocks * BLOCK_LENGTH + 1)
+        except asyncio.IncompleteReadError:
+            raise ValueError(f"{addr}: invalid number of blocks: {num_blocks}")
 
         if c[-1] != 0:
-            raise ValueError("Message does not end with 0")
+            raise ValueError(f"{addr}: message does not end with 0")
 
         if oracle(c[:-1]):
-            conn.send(b"1\x00")
+            ans = b"1\x00"
         else:
-            conn.send(b"0\x00")
+            ans = b"0\x00"
+
+        writer.write(ans)
+        await writer.drain()
+
+    print("Closing connection from", addr)
 
 
-def answer(k: bytes, conn: socket.socket):
-    def oracle(c: bytes) -> bool:
-        return valid_cipher(k, c)
-
-    try:
-        send_answer(conn, oracle)
-    finally:
-        conn.close()
-
-
-def main():
+async def main():
     k = read_key()
 
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    async def cb(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        def oracle(c: bytes) -> bool:
+            return valid_cipher(k, c)
+
+        await handle_oracle(oracle, reader, writer)
 
     ip = "0.0.0.0"
     port = 8080
 
-    soc.bind((ip, port))
+    s = await asyncio.start_server(cb, ip, port)
     print(f"Listening on {ip}:{port}")
-    soc.listen()
 
-    while True:
-        conn, (addr, port) = soc.accept()
-        print(f"Opened connection from {addr}")
-        t = Thread(target=answer, args=(k, conn))
-        t.start()
+    async with s:
+        await s.serve_forever()
 
 
-def read_key():
-    try:
-        hex_key = os.environ["MC_PADDING_KEY"]
-
-        k = bytes.fromhex(hex_key)
-        print(f"Using configured key:", k.hex())
-
-        assert len(k) == 16
-
-    except KeyError:
-        k = token_bytes(16)
-        print(f"Using random key", k.hex())
-
-    message = os.environ.get("MC_PADDING_MESSAGE", None)
-    if message:
-        padded = pad(message.encode())
-        print("Encrypted message:", encrypt(k, padded).hex())
-
-    return k
+def wrapper():
+    asyncio.run(main())
 
 
 if __name__ == "__main__":
-    main()
+    wrapper()
